d8b0c3a0423b17aaeabf486d5c753205da799d73
Aasif Rehman <71745081+Asif-rehman012@users.noreply.github.com>
2026-01-05 22:53:20 +0530

    freecode camp tests

---
+++ freecodecamp/basic.py
@@ -0,0 +1,42 @@
+# var =  input("enter data")
+# print(var)
+# print(type(var))
+
+# print("Hello","Aasif","cse",sep="__")
+
+
+# print('Aasif rehman')
+# a=10
+# print(a)
+# print (type(a))
+
+# a=80-74
+# print(a)
+# print(type(a))
+
+# a= True
+# print(a)
+
+# a={"Asif", 77,78.44}
+# print(a)
+# print(type(a))
+
+
+a=[12,'asif',21.4]
+print(a) 
+print (type(a))
+
+
+a=(12,'asif',21.4)
+print(a) 
+print (type(a))
+
+
+a={12,'asif',21.4}
+print(a) 
+print (type(a))
+
+
+a={'age': 23}
+print(a) 
+print (type(a))
